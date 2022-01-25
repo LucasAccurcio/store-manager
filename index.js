8520ce9403bc248742f152698b6cc0a284964e87
LucasAccurcio <lucas.accurcio@gmail.com>
2022-01-25 17:19:33 -0300

    Iniciando o projeto

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 
+const express = require('express');
+// const bodyParser = require('body-parser');
+// const rescue = require('express-rescue');
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Escutando na porta ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
